64879f08c440ac66a58baeea8addead45492c481
Aarthif-Nawaz <mohammed.2017313@iit.ac.lk>
2021-05-03 15:33:11 -0800

    Neww Changes

---
+++ build/precache-manifest.12b14d27cb17d3a67a2d622486cdf137.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb133129aca632eebde7c2a23587555f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faf4559563011e88b844",
+    "url": "/static/css/2.d9e2e5c3.chunk.css"
+  },
+  {
+    "revision": "1e6ea2ef75fb6992e63a",
+    "url": "/static/css/main.ff21dc56.chunk.css"
+  },
+  {
+    "revision": "faf4559563011e88b844",
+    "url": "/static/js/2.f563942d.chunk.js"
+  },
+  {
+    "revision": "7c3d6953881395763e8f11409a8b3690",
+    "url": "/static/js/2.f563942d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6ea2ef75fb6992e63a",
+    "url": "/static/js/main.8f89327a.chunk.js"
+  },
+  {
+    "revision": "5aa4e40ef8f6b26c603a",
+    "url": "/static/js/runtime-main.5b7bd79d.js"
+  },
+  {
+    "revision": "24cee876c703c734950d5fdf95fc0c00",
+    "url": "/static/media/logo.24cee876.png"
+  }
+]);
